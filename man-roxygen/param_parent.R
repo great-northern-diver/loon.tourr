e8a338743bb6f8a79fb097d625113e434bc703e2
R. Wayne Oldford <rwoldford@users.noreply.github.com>
2021-03-20 22:24:26 -0400

    Add files via upload

---
+++ man-roxygen/param_parent.R
@@ -0,0 +1,4 @@
+#' @param parent a valid Tk parent widget path. When the parent widget is
+#'   specified (i.e. not \code{NULL}) then the plot widget needs to be placed using
+#'   some geometry manager like \code{\link{tkpack}} or \code{\link{tkplace}} in
+#'   order to be displayed. See the examples below.
